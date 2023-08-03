c040c424639f670bfa96789fb20035482c6c4d4c
hüseyin gülerman <huseyingulerman.1997@gmail.com>
2023-08-03 11:09:07 +0300

    7.Ders

--- EasyCashIdentityProject.DtoLayer/Dtos/AppUserDtos/AppUserRegisterDto.cs
+++ EasyCashIdentityProject.DtoLayer/Dtos/AppUserDtos/AppUserRegisterDto.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace EasyCashIdentityProject.DtoLayer.Dtos.AppUserDtos
 {
-    internal class AppUserRegisterDto
+    public class AppUserRegisterDto
     {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
     }
 }
+//Ad,soyad,username,mail,password,confirmpassword
