24a5e291ea66f94e5f1edc99e62aa6324997c457
hüseyin gülerman <huseyingulerman.1997@gmail.com>
2023-08-03 15:54:49 +0300

    9.Ders Tema Dosyaları

---
+++ EasyCashIdentityProject.BusinessLayer/Abstract/IGenericService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.BusinessLayer.Abstract
+{
+    public interface IGenericService<T> where T: class
+    {
+        void TInsert(T entity);
+        void TUpdate(T entity);
+        void TDelete(T entity);
+        T TGetById(int id);
+        List<T> TGetList();
+    }
+}
---
+++ EasyCashIdentityProject.BusinessLayer/Concrete/CustomerAccountManager.cs
@@ -0,0 +1,46 @@
+﻿using EasyCashIdentityProject.BusinessLayer.Abstract;
+using EasyCashIdentityProject.DataAccessLayer.Abstract;
+using EasyCashIdentityProject.Entitylayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.BusinessLayer.Concrete
+{
+    public class CustomerAccountManager : ICustomerAccountService
+    {
+        private readonly ICustomerAccountDal _customerAccountDal;
+
+        public CustomerAccountManager(ICustomerAccountDal customerAccountDal)
+        {
+            _customerAccountDal=customerAccountDal;
+        }
+
+        public void TDelete(CustomerAccount entity)
+        {
+            _customerAccountDal.Delete(entity);
+        }
+
+        public CustomerAccount TGetById(int id)
+        {
+            return _customerAccountDal.GetById(id);
+        }
+
+        public List<CustomerAccount> TGetList()
+        {
+          return _customerAccountDal.GetList();
+        }   
+
+        public void TInsert(CustomerAccount entity)
+        {
+            _customerAccountDal.Insert(entity);
+        }
+
+        public void TUpdate(CustomerAccount entity)
+        {
+            _customerAccountDal.Update(entity);
+        }
+    }
+}
